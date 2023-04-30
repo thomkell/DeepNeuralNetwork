e3609be256808f489ac8653f2376337e1cad6e9d
Anuradha Agarwal <98137482+anuradha1901@users.noreply.github.com>
2023-04-30 09:55:38 -0700

    Update cuda.cu

--- comparision/cuda.cu
+++ comparision/cuda.cu
@@ -1,3 +1,5 @@
+// CUDA code for only forward propagation 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -8,27 +10,14 @@
 #define INPUT_SIZE 4800
 #define HIDDEN_SIZE 4800
 #define OUTPUT_SIZE 1
-// #define LEARNING_RATE 0.01
 #define NUM_EPOCHS 1
 #define numTrain 455
 #define NUM_THREADS 1
-
-// error handler defintion
-// #define HANDLE_ERROR( err ) ( HandleError( err , _FILE, __LINE_ ) )
-// static void HandleError(cudaError_t err , const char *file , int line)
-// {
-//     if (err != cudaSuccess)
-//     {
-//         printf("%s in %s at line %d\n", cudaGetErrorString(err),file , line);
-//         exit (EXIT_FAILURE) ;
-//     } 
-// }
 
 // Sigmoid activation function
 __device__ double sigmoid(double x) {
     return 1.0 / (1.0 + exp(-x));
 }
-
 
 // Forward propagation kernel
 __global__ void forward_kernel(double* X, double* W1, double* W2, double* W3, double* b1, double* b2, double* b3, double* hidden1, double* hidden2, double* output) {
@@ -66,12 +55,9 @@ __global__ void forward_kernel(double* X, double* W1, double* W2, double* W3, do
 
 int main(int argc, char *argv[]) {
 
-
-
     double X[numTrain][INPUT_SIZE];
 
     // Initialize input data
-
     for (int i = 0; i < numTrain; i++) {
         for (int j = 0; j < INPUT_SIZE ; j++) {
             X[i][j] = ((double) rand() / RAND_MAX) * 2.0 - 1.0;
@@ -83,16 +69,14 @@ int main(int argc, char *argv[]) {
             y[i] = ((double) rand() / RAND_MAX) * 2.0 - 1.0;
     }
 
-
-
     double *h_W1 = (double *)malloc(INPUT_SIZE * HIDDEN_SIZE * sizeof(double));
-    double *h_W2 = (double *)malloc(HIDDEN_SIZE * sizeof(double));
+    double *h_W2 = (double *)malloc(HIDDEN_SIZE * HIDDEN_SIZE * sizeof(double));
     double *h_W3 = (double *)malloc(HIDDEN_SIZE * sizeof(double));
 
     for (int i = 0; i < INPUT_SIZE * HIDDEN_SIZE; i++) {
         h_W1[i] = (double)rand() / RAND_MAX;
     }
-    for (int i = 0; i < HIDDEN_SIZE; i++) {
+    for (int i = 0; i < HIDDEN_SIZE * HIDDEN_SIZE; i++) {
         h_W2[i] = (double)rand() / RAND_MAX;
     }
     for (int i = 0; i < HIDDEN_SIZE; i++) {
@@ -115,22 +99,13 @@ int main(int argc, char *argv[]) {
         h_b3[i] = (double)rand() / RAND_MAX;
     }
 
-    
-
-    // Measure time taken for training
-    // cudaEvent_t start, end;
-    // float total;
-    // cudaEventCreate(&start);
-    // cudaEventCreate(&end);
-    // cudaEventRecord(start,0);
-
     struct timeval t1, t2;
     gettimeofday(&t1, 0);
 
     // Initialize weights
     double *d_W1, *d_W2, *d_W3;
     cudaMalloc(&d_W1, INPUT_SIZE * HIDDEN_SIZE * sizeof(double));
-    cudaMalloc(&d_W2, HIDDEN_SIZE * sizeof(double));
+    cudaMalloc(&d_W2, HIDDEN_SIZE * HIDDEN_SIZE * sizeof(double));
     cudaMalloc(&d_W3, HIDDEN_SIZE * sizeof(double));
 
     // Initialize weights
@@ -140,13 +115,11 @@ int main(int argc, char *argv[]) {
     cudaMalloc(&d_b3, OUTPUT_SIZE * sizeof(double));
 
     cudaMemcpy(d_W1, h_W1, INPUT_SIZE * HIDDEN_SIZE * sizeof(double), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_W2, h_W2, HIDDEN_SIZE * sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_W2, h_W2, HIDDEN_SIZE * HIDDEN_SIZE * sizeof(double), cudaMemcpyHostToDevice);
     cudaMemcpy(d_W3, h_W3, HIDDEN_SIZE * sizeof(double), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b1, h_b1, INPUT_SIZE * HIDDEN_SIZE * sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b1, h_b1, INPUT_SIZE * sizeof(double), cudaMemcpyHostToDevice);
     cudaMemcpy(d_b2, h_b2, HIDDEN_SIZE * sizeof(double), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b3, h_b3, HIDDEN_SIZE * sizeof(double), cudaMemcpyHostToDevice);
-
-    
+    cudaMemcpy(d_b3, h_b3, HIDDEN_SIZE * sizeof(double), cudaMemcpyHostToDevice); 
 
     // Training loop
     for (int epoch = 0; epoch < NUM_EPOCHS; epoch++) {
@@ -162,41 +135,10 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    // cudaEventRecord(end,0);
-    // cudaEventSynchronize(end);
-    // cudaEventElapsedTime(&total, start, end);
+
     gettimeofday(&t2, 0);
     double time = (1000000.0*(t2.tv_sec-t1.tv_sec) + t2.tv_usec-t1.tv_usec)/1000.0;
     printf("Time to generate:  %10.3f ms \n", time);
-
-    
-
-    
-
-    // Print final weights
-    // printf("Final weights W1: \n");
-    // for (int i = 0; i < INPUT_SIZE * HIDDEN_SIZE; i++) {
-    //     printf("%.4f ", h_W1[i]);
-    // }
-    // printf("\n");
-    // printf("Final weights W2: \n");
-    // for (int i = 0; i < HIDDEN_SIZE; i++) {
-    //     printf("%.4f ", h_W2[i]);
-    // }
-    // printf("\n");
-
-    //HANDLE_ERROR(cudaDeviceSynchronize());
-
-    
-    
-
-    // Calculate elapsed time for 
-    //HANDLE_ERROR(cudaEventRecord(stop,0));
-    //HANDLE_ERROR(cudaEventSynchronize(stop));
-    //float elapsed_time;
-    //HANDLE_ERROR(cudaEventElapsedTime(&elapsed_time, start, stop));
-
-    //printf("Training completed in %.4f seconds.\n", elapsed_time);
 
     // Free memory
     cudaFree(d_W1);
